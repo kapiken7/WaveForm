749b73cf400b3ff552dffac1b7bf7c011b793279
tokyo-study060 <tokyo-study060@SUSR-25020071>
2025-11-17 14:46:23 +0900

    UIレイアウトの改善とコードのリファクタリング
    - グラフ関連の変数名を直感的な命名に変更
    - ボタンの位置とサイズを調整し、操作性を向上
    - ラベルを`TableLayoutPanel`内に再配置し、レイアウトを整理
    - 新しい`TableLayoutPanel`と`GroupBox`を追加し、統計情報を視覚的にグループ化
    - コントロールの再配置により、UI構造を整理
    - `SuspendLayout`と`ResumeLayout`間に初期化コードを追加
    - `PerformLayout`を削除し、コードの簡素化を実施

--- WaveForm/Form1.Designer.cs
+++ WaveForm/Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             StartButton = new Button();
             StopButton = new Button();
             label1 = new Label();
@@ -42,14 +42,18 @@
             label6 = new Label();
             label7 = new Label();
             label8 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            groupBox1 = new GroupBox();
             ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // StartButton
             // 
-            StartButton.Location = new Point(83, 39);
+            StartButton.Location = new Point(39, 39);
             StartButton.Name = "StartButton";
-            StartButton.Size = new Size(94, 29);
+            StartButton.Size = new Size(129, 41);
             StartButton.TabIndex = 0;
             StartButton.Text = "Start";
             StartButton.UseVisualStyleBackColor = true;
@@ -57,9 +61,9 @@
             // 
             // StopButton
             // 
-            StopButton.Location = new Point(218, 39);
+            StopButton.Location = new Point(188, 39);
             StopButton.Name = "StopButton";
-            StopButton.Size = new Size(94, 29);
+            StopButton.Size = new Size(129, 41);
             StopButton.TabIndex = 1;
             StopButton.Text = "Stop";
             StopButton.UseVisualStyleBackColor = true;
@@ -68,35 +72,37 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(387, 24);
+            label1.Location = new Point(3, 0);
             label1.Name = "label1";
-            label1.Size = new Size(69, 20);
+            label1.Padding = new Padding(0, 5, 0, 5);
+            label1.Size = new Size(54, 30);
             label1.TabIndex = 2;
-            label1.Text = "現在値：";
+            label1.Text = "現在値";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(462, 24);
+            label2.Location = new Point(72, 0);
             label2.Name = "label2";
-            label2.Size = new Size(17, 20);
+            label2.Padding = new Padding(0, 5, 0, 5);
+            label2.Size = new Size(17, 30);
             label2.TabIndex = 3;
             label2.Text = "0";
             // 
             // chart1
             // 
-            chartArea3.Name = "ChartArea1";
-            chart1.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            chart1.Legends.Add(legend3);
-            chart1.Location = new Point(39, 92);
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(39, 95);
             chart1.Name = "chart1";
-            series3.BorderWidth = 5;
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            chart1.Series.Add(series3);
+            series1.BorderWidth = 5;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
             chart1.Size = new Size(724, 346);
             chart1.TabIndex = 4;
             chart1.Text = "chart1";
@@ -104,78 +110,112 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(387, 59);
+            label3.Location = new Point(3, 33);
             label3.Name = "label3";
-            label3.Size = new Size(69, 20);
+            label3.Padding = new Padding(0, 5, 0, 5);
+            label3.Size = new Size(54, 30);
             label3.TabIndex = 5;
-            label3.Text = "平均値：";
+            label3.Text = "平均値";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(462, 59);
+            label4.Location = new Point(72, 33);
             label4.Name = "label4";
-            label4.Size = new Size(28, 20);
+            label4.Padding = new Padding(0, 5, 0, 5);
+            label4.Size = new Size(28, 30);
             label4.TabIndex = 6;
             label4.Text = "0.0";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(525, 24);
+            label5.Location = new Point(141, 0);
             label5.Name = "label5";
-            label5.Size = new Size(69, 20);
+            label5.Padding = new Padding(0, 5, 0, 5);
+            label5.Size = new Size(54, 30);
             label5.TabIndex = 7;
-            label5.Text = "最大値：";
+            label5.Text = "最大値";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(600, 24);
+            label6.Location = new Point(210, 0);
             label6.Name = "label6";
-            label6.Size = new Size(17, 20);
+            label6.Padding = new Padding(0, 5, 0, 5);
+            label6.Size = new Size(17, 30);
             label6.TabIndex = 8;
             label6.Text = "0";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(525, 59);
+            label7.Location = new Point(141, 33);
             label7.Name = "label7";
-            label7.Size = new Size(69, 20);
+            label7.Padding = new Padding(0, 5, 0, 5);
+            label7.Size = new Size(54, 30);
             label7.TabIndex = 9;
-            label7.Text = "最小値：";
+            label7.Text = "最小値";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(600, 59);
+            label8.Location = new Point(210, 33);
             label8.Name = "label8";
-            label8.Size = new Size(17, 20);
+            label8.Padding = new Padding(0, 5, 0, 5);
+            label8.Size = new Size(17, 30);
             label8.TabIndex = 10;
             label8.Text = "0";
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 4;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(label2, 1, 0);
+            tableLayoutPanel1.Controls.Add(label4, 1, 1);
+            tableLayoutPanel1.Controls.Add(label5, 2, 0);
+            tableLayoutPanel1.Controls.Add(label6, 3, 0);
+            tableLayoutPanel1.Controls.Add(label7, 2, 1);
+            tableLayoutPanel1.Controls.Add(label8, 3, 1);
+            tableLayoutPanel1.Controls.Add(label3, 0, 1);
+            tableLayoutPanel1.Location = new Point(40, 18);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(278, 67);
+            tableLayoutPanel1.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(tableLayoutPanel1);
+            groupBox1.Location = new Point(341, 1);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(353, 91);
+            groupBox1.TabIndex = 12;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "モニタ値（統計値は直近20件）";
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(label8);
-            Controls.Add(label7);
-            Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
             Controls.Add(chart1);
-            Controls.Add(label2);
-            Controls.Add(label1);
             Controls.Add(StopButton);
             Controls.Add(StartButton);
+            Controls.Add(groupBox1);
             Name = "MainForm";
             Text = "MainForm";
             ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            groupBox1.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -191,5 +231,7 @@
         private Label label6;
         private Label label7;
         private Label label8;
+        private TableLayoutPanel tableLayoutPanel1;
+        private GroupBox groupBox1;
     }
 }
